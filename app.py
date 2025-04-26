61b100364c925cbfacbe4b303938eb79c041168d
LuckyRampei <suwahhau@gmail.com>
2025-04-26 21:43:54 +0530

    corrected nltk

--- app.py
+++ app.py
@@ -5,9 +5,19 @@ from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords 
 from nltk.stem.porter import PorterStemmer
 
-# Download necessary NLTK data
-nltk.download('punkt')
-nltk.download('stopwords')
+# Download necessary NLTK resources
+import os
+nltk_data_path = os.path.join(os.getcwd(), 'nltk_data')
+nltk.data.path.append(nltk_data_path)
+try:
+    nltk.data.find('tokenizers/punkt')
+except LookupError:
+    nltk.download('punkt', download_dir=nltk_data_path)
+
+try:
+    nltk.data.find('corpora/stopwords')
+except LookupError:
+    nltk.download('stopwords', download_dir=nltk_data_path)
 
 # Initialize stemmer
 ps = PorterStemmer()
